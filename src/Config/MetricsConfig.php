4503502233822c4df5bcecd1e6493b96feb9d821
Vladimir <vladimir@sobaka.com>
2024-08-26 19:04:01 +0200

    init

---
+++ src/Config/MetricsConfig.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Anodio\Metrics\Config;
+
+use Anodio\Core\AttributeInterfaces\AbstractConfig;
+use Anodio\Core\Attributes\Config;
+
+#[Config('metrics')]
+class MetricsConfig extends AbstractConfig
+{
+
+}
---
+++ src/ServiceProviders/MetricsServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Anodio\Metrics\ServiceProviders;
+use Anodio\Core\AttributeInterfaces\ServiceProviderInterface;
+
+class MetricsServiceProvider implements ServiceProviderInterface
+{
+
+    public function register(\DI\ContainerBuilder $containerBuilder): void
+    {
+
+    }
+}
