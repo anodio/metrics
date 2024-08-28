faae6c4f82460504bfdd0b26df09e63832b563ca
Vladimir <vladimir@sobaka.com>
2024-08-28 14:29:43 +0200

    basic metrics component

--- src/ServiceProviders/MetricsServiceProvider.php
+++ src/ServiceProviders/MetricsServiceProvider.php
@@ -2,12 +2,22 @@
 
 namespace Anodio\Metrics\ServiceProviders;
 use Anodio\Core\AttributeInterfaces\ServiceProviderInterface;
+use Anodio\Core\Attributes\ServiceProvider;
+use DI\ContainerBuilder;
+use Prometheus\CollectorRegistry;
+use Prometheus\Storage\InMemory;
 
+#[ServiceProvider]
 class MetricsServiceProvider implements ServiceProviderInterface
 {
 
-    public function register(\DI\ContainerBuilder $containerBuilder): void
+    public function register(ContainerBuilder $containerBuilder): void
     {
-
+        $containerBuilder->addDefinitions([
+            InMemory::class => \Di\create(InMemory::class),
+            CollectorRegistry::class =>
+                \DI\create(CollectorRegistry::class)
+                ->constructor(\Di\get(InMemory::class))
+        ]);
     }
 }
