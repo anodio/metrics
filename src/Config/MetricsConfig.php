2a2291eefb924c3aeb51cdbb2371c662554ee3ab
Vladimir <vladimir@sobaka.com>
2024-09-04 17:02:41 +0200

    metrics config

--- src/Config/MetricsConfig.php
+++ src/Config/MetricsConfig.php
@@ -4,9 +4,12 @@ namespace Anodio\Metrics\Config;
 
 use Anodio\Core\AttributeInterfaces\AbstractConfig;
 use Anodio\Core\Attributes\Config;
+use Anodio\Core\Configuration\Env;
+use Prometheus\Storage\InMemory;
 
 #[Config('metrics')]
 class MetricsConfig extends AbstractConfig
 {
-
+    #[Env('METRICS_STORAGE_DRIVER', InMemory::class)]
+    public string $metricsStorageDriver;
 }
