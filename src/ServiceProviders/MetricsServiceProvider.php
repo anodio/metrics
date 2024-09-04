1d76a30d739846f617c947fb5e6ad8bb40057193
Vladimir <vladimir@sobaka.com>
2024-09-04 17:03:01 +0200

    drivers for metrics

--- src/ServiceProviders/MetricsServiceProvider.php
+++ src/ServiceProviders/MetricsServiceProvider.php
@@ -3,6 +3,10 @@
 namespace Anodio\Metrics\ServiceProviders;
 use Anodio\Core\AttributeInterfaces\ServiceProviderInterface;
 use Anodio\Core\Attributes\ServiceProvider;
+use Anodio\Http\Config\WorkerConfig;
+use Anodio\Metrics\Config\MetricsConfig;
+use Anodio\Metrics\Drivers\SupervisorMetricsHttpProxyDriver;
+use Anodio\Metrics\Drivers\SupervisorMetricsWorkerDriver;
 use DI\ContainerBuilder;
 use Prometheus\CollectorRegistry;
 use Prometheus\Storage\InMemory;
@@ -10,14 +14,34 @@ use Prometheus\Storage\InMemory;
 #[ServiceProvider]
 class MetricsServiceProvider implements ServiceProviderInterface
 {
-
     public function register(ContainerBuilder $containerBuilder): void
     {
+        if (str_starts_with(CONTAINER_NAME, 'worker')) {
+            $containerBuilder->addDefinitions([
+                \Prometheus\Storage\Adapter::class => \Di\create(SupervisorMetricsWorkerDriver::class)
+                ->constructor(\Di\get(WorkerConfig::class)),
+            ]);
+        }
+        if (str_starts_with(CONTAINER_NAME, 'http-proxy')) {
+            $containerBuilder->addDefinitions([
+                \Prometheus\Storage\Adapter::class => \Di\create(SupervisorMetricsHttpProxyDriver::class),
+            ]);
+        }
+        if (str_starts_with(CONTAINER_NAME, 'cli')) {
+            $containerBuilder->addDefinitions([
+                \Prometheus\Storage\Adapter::class => \Di\factory(function(MetricsConfig $metricsConfig) {
+                    if ($metricsConfig->metricsStorageDriver === InMemory::class) {
+                        return new InMemory();
+                    } else {
+                        throw new \Exception('Unknown metrics storage driver: '.$metricsConfig->metricsStorageDriver);
+                    }
+                })->parameter('metricsConfig', \Di\get(MetricsConfig::class)),
+            ]);
+        }
         $containerBuilder->addDefinitions([
-            InMemory::class => \Di\create(InMemory::class),
             CollectorRegistry::class =>
                 \DI\create(CollectorRegistry::class)
-                ->constructor(\Di\get(InMemory::class))
+                    ->constructor(\Di\get(\Prometheus\Storage\Adapter::class))
         ]);
     }
 }
